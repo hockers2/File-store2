6bbd8230255bad70efa2ebc984ac2641c1cb1c4b
hockers2 <84176204+hockers2@users.noreply.github.com>
2023-10-13 13:12:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,7 +116,7 @@ class Bot(Client):
 
         self.set_parse_mode("html")
         self.LOGGER(__name__).info(
-            f"[🔥 BERHASIL DIAKTIFKAN! 🔥]\n\nBOT Dibuat oleh @{OWNER}\nJika @{OWNER} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+            f"[🔥 SUCCESSFULLY ACTIVATED! 🔥]\n\nBOT Created by @{OWNER}\nIf @{OWNER} Needs Help, Please Ask in the Group https://t.me/SharingUserbot"
         )
 
     async def stop(self, *args):
